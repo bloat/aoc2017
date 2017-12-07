5897bc24b5d1cde4f6291dabbff67c694379cdd7
Andrew Cowper <andrew.cowper@slothrop.net>
2017-12-07 18:14:09 +0000

    Day 7 part 2

--- src/net/slothrop/aoc2017/day7.clj
+++ src/net/slothrop/aoc2017/day7.clj
@@ -9,17 +9,17 @@
   (with-open [r (io/reader (io/resource "day7-example.txt"))]
     (into [] (line-seq r))))
 
-(defrecord Node [name weight children])
+(defrecord Node [name weight children total-weight])
 
 (defn add-node
   ([nodes name]
    (if (contains? nodes name)
      nodes
-     (assoc nodes name (Node. name nil nil))))
+     (assoc nodes name (Node. name nil nil nil))))
   ([nodes name weight]
    (if (contains? nodes name)
      (assoc-in nodes [name :weight] weight)
-     (assoc nodes name (Node. name weight nil)))))
+     (assoc nodes name (Node. name weight nil nil)))))
 
 (defn get-name [^String line]
   (keyword (.substring line 0 (.indexOf line (int \space)))))
@@ -55,9 +55,49 @@
           (into #{} (keys nodes))
           (into #{} (mapcat :children (vals nodes))))))
 
-(comment
-  (def exnodes (load-nodes example))
-  (def mynodes (load-nodes input))
-  (base-node mynodes)
+;; Part 2
 
-  (map get-children example))
+(defn total-node-weight [nodes node]
+  (if (:total-weight (nodes node))
+    nodes
+
+    (if (empty? (:children (nodes node)))
+      (assoc-in nodes [node :total-weight] (:weight (nodes node)))
+      (let [nodes-with-populated-children
+            (reduce total-node-weight
+                    nodes
+                    (:children (nodes node)))]
+        (assoc-in nodes-with-populated-children [node :total-weight]
+                  (reduce +
+                          (:weight (nodes-with-populated-children node))
+                          (map (fn [child] (:total-weight (nodes-with-populated-children child)))
+                               (:children (nodes-with-populated-children node)))))))))
+
+(defn bad-child [nodes node]
+  (let [children (:children (nodes node))]
+    (when (> (count children) 2)
+      (->> children
+           (map (fn [child] [child (:total-weight (nodes child))]))
+           (group-by second)
+           (filter (fn [[k v]] (= 1 (count v))))
+           first
+           second
+           first
+           first))))
+
+
+(defn balanced? [nodes node]
+  (= 1 (count (into #{} ))))
+
+(comment
+  (def exnodes (let [n (load-nodes example)]
+                 (total-node-weight n (base-node n))))
+
+  (def mynodes (let [n (load-nodes input)]
+                 (total-node-weight n (base-node n))))
+
+  (base-node mynodes)
+  (take-while identity (iterate #(bad-child mynodes %) :svugo))
+  (map (fn [c] (:total-weight (mynodes c))) (:children (mynodes :yruivis)))
+  (mynodes :sphbbz)
+  )
